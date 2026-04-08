95b6d5357026c0d384c9771dd50ea828a15f1807
brenne <brenneitsouhou9@gmail.com>
2026-04-08 09:44:58 +0100

    feat: configure SharedModule with reusable components

--- frontend/annonces-frontend/src/app/app.routes.ts
+++ frontend/annonces-frontend/src/app/app.routes.ts
@@ -1,3 +1,58 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'annonces',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'annonces',
+    loadChildren: () =>
+      import('./features/annonces/annonces-module')
+        .then(m => m.AnnoncesModule)
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth-module')
+        .then(m => m.AuthModule)
+  },
+
+  {
+    path: 'recherche',
+    loadChildren: () =>
+      import('./features/recherche/recherche-module')
+        .then(m => m.RechercheModule)
+  },
+
+  {
+    path: 'recommandations',
+    loadChildren: () =>
+      import('./features/recommandations/recommandations-module')
+        .then(m => m.RecommandationsModule)
+  },
+
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard-module')
+        .then(m => m.DashboardModule)
+  },
+
+  {
+    path: 'chatbot',
+    loadChildren: () =>
+      import('./features/chatbot/chatbot-module')
+        .then(m => m.ChatbotModule)
+  },
+
+  {
+    path: '**',
+    redirectTo: 'annonces'
+  }
+
+];
--- frontend/annonces-frontend/src/app/shared/shared-module.ts
+++ frontend/annonces-frontend/src/app/shared/shared-module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+import { CardAnnonce } from './components/card-annonce/card-annonce';
+import { SearchBar } from './components/search-bar/search-bar';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Navbar,
+    Footer,
+    CardAnnonce,
+    SearchBar
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    Navbar,
+    Footer,
+    CardAnnonce,
+    SearchBar,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
